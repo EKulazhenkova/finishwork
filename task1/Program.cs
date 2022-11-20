e8782795682a73de13942dd2609a23444a549ac4
Kulazhenkova Ekaterina <kulazhenkova.k@gmail.com>
2022-11-20 13:58:31 +0300

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введи M: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи N: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"M={M}, N={N}");
+if (N>M)
+for (int i = M; i <= N; i++)
+    Console.Write($" {i}");
+else
+    for (int i = N; i <= M; i++)
+        Console.Write($" {i}");
---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿int InputNumber(string input)
+{
+Console.Write(input);
+int output = Convert.ToInt32(Console.ReadLine());
+return output;
+}
+int Akkerman(int n, int m)
+{
+if (n == 0)
+{
+return m + 1;
+}
+else if (n > 0 && m == 0)
+{
+return Akkerman(n - 1, 1);
+}
+else
+{
+return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+}
+
+int m = InputNumber("Ввод m: ");
+int n = InputNumber("Ввод n: ");
+int akkermanFunction = Akkerman(n, m);
+Console.Write($"m = {m}, n = {n} - > A(m,n) = {akkermanFunction} ");
+    
